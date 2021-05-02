5a250184d5c92ed2774a92c7987d4891d99a1283
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-05-02 07:53:36 +0530

    Added socket.io

--- client/main.js
+++ client/main.js
@@ -1,11 +1,33 @@
 import Quill from 'quill';
+import { io } from 'socket.io-client';
 import 'quill/dist/quill.snow.css';
 import './styles.css';
 
+const SERVER_URL = 'http://localhost:3000';
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['bold', 'italic', 'underline'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ align: [] }],
+    ['image', 'blockquote', 'code-block'],
+    ['clean']
+];
+
 function init() {
     const quill = new Quill('#editor', {
-        theme: 'snow'
+        theme: 'snow',
+        modules: {
+            toolbar: TOOLBAR_OPTIONS
+        }
     });
+
+    const socket = io(SERVER_URL);
+
+    
 }
 
 init();
--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
 const socketio = require('socket.io');
 
 const SOCKET_PORT = process.env.SOCKET_PORT || 3000;
-const server = socketio(SOCKET_PORT);
+const io = socketio(SOCKET_PORT, {
+    cors: {
+        origin: 'http://localhost:1234',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', socket => {
+    console.log('new connection');
+});
