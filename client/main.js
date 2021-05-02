8d0e9fbf1ca4108ecd59003bb9c92e1180023358
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-05-02 08:31:35 +0530

    Added rooms

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import Quill from 'quill';
 import { io } from 'socket.io-client';
+import { nanoid } from 'nanoid';
 import 'quill/dist/quill.snow.css';
 import './styles.css';
 
@@ -18,26 +19,46 @@ const TOOLBAR_OPTIONS = [
 ];
 
 function init() {
-    const quill = new Quill('#editor', {
-        theme: 'snow',
-        modules: {
-            toolbar: TOOLBAR_OPTIONS
-        }
-    });
+    const path = window.location.pathname;
+    
+    if (path === '/') {
+        const randomDocumentId = nanoid(6);
 
-    const socket = io(SERVER_URL);
+        window.location.pathname = randomDocumentId;
+    } else {
+        const documentId = path.slice(1);
 
-    // listen for text changes from client and send to server
-    quill.on('text-change', (delta, oldDelta, source) => {
-        if (source !== 'user') return;
+        const quill = new Quill('#editor', {
+            theme: 'snow',
+            modules: {
+                toolbar: TOOLBAR_OPTIONS
+            }
+        });
 
-        socket.emit('client-changes', delta);
-    });
+        quill.disable();
+        quill.setText('Loading document....');
+    
+        const socket = io(SERVER_URL);
 
-    // listen for changes from server and update client
-    socket.on('server-changes', delta => {
-        quill.updateContents(delta);
-    });
+        socket.emit('get-document', documentId);
+
+        socket.once('load-document', document => {
+            quill.setContents(document);
+            quill.enable();
+        });
+    
+        // listen for text changes from client and send to server
+        quill.on('text-change', (delta, oldDelta, source) => {
+            if (source !== 'user') return;
+    
+            socket.emit('client-changes', delta);
+        });
+    
+        // listen for changes from server and update client
+        socket.on('server-changes', delta => {
+            quill.updateContents(delta);
+        });
+    }
 }
 
 init();
--- server/server.js
+++ server/server.js
@@ -3,13 +3,21 @@ const socketio = require('socket.io');
 const SOCKET_PORT = process.env.SOCKET_PORT || 3000;
 const io = socketio(SOCKET_PORT, {
     cors: {
-        origin: 'http://localhost:1234',
+        origin: 'http://localhost:5000',
         methods: ['GET', 'POST']
     }
 });
 
 io.on('connection', socket => {
-    socket.on('client-changes', delta => {
-        socket.broadcast.emit('server-changes', delta);
+    socket.on('get-document', documentId => {
+        const data = '';
+
+        socket.join(documentId);
+
+        socket.emit('load-document', data);
+
+        socket.on('client-changes', delta => {
+            socket.broadcast.to(documentId).emit('server-changes', delta);
+        });
     });
 });
